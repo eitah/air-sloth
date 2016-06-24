e6b54220374c457a63254a5e40075e7184db0b32
Jayam Subramanyam and Rodolfo Ballesteros <pair+jayam+rodolfo@allstate.com>
2016-06-24 15:38:55 -0500

    fix issue

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,8 +10,4 @@ router.get('/', (req, res) => {
   country.find((err, country) => {
    res.render('user/index', { country });
  });
-
-// res.render('user/index', {countries: [{name: 'brazil' , picture: 'http://kids.nationalgeographic.com/content/dam/kids/photos/Countries/A-G/brazil-christ-the-redeemer.jpg'},
-// {name: 'mexico' , picture: 'http://i2.cdn.turner.com/cnnnext/dam/assets/151011072406-insiderguide-mexicocity-main-exlarge-169.jpg'},
-// {name: 'Japan' , picture: 'https://www.idp.com/global/-/media/IDP/Global/Destinations/US/New-York-USA.ashx?h=580&w=960&la=en&hash=79B07D830CCB09627DEA35B0CF56FA390E15B6F8'}
-// ]});
+ });
